6f7ad44afc0797a4cb34e0b6dbb8024ace40f039
Mariana Werneck <marinhomariana8@gmail.com>
2022-10-25 17:45:33 -0300

    Requisito 1 - Criando migrations para as tabelas users, categories, blog_posts e posts_categories

---
+++ src/migrations/20221025175547-create_posts.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: Sequelize.STRING,
+      content: Sequelize.STRING,
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },
+      published: Sequelize.DATE,
+      updated: Sequelize.DATE,
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
