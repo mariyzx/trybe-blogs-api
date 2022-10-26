f5626f436fa788308314af4c4ffd8aa682313aa8
Mariana Werneck <marinhomariana8@gmail.com>
2022-10-26 16:02:21 -0300

    Requisito 7 - Criando o model Category

---
+++ src/models/Category.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+  });
+
+  return Category;
+}
